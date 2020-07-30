c716d8385251f7a8ffbc70aa6bef5c8c12242e67
Nicole Lasusa <nicolelasusa@Nicoles-Air.fios-router.home>
2020-07-30 18:53:59 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// NAV BAR TOGGLE 
+function classToggle() {
+    const navs = document.querySelectorAll('.Navbar__Items')
+    
+    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+  }
+  
+  document.querySelector('.Navbar__Link-toggle')
+    .addEventListener('click', classToggle);
