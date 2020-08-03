adc4c7a923f610e82dc1452b7c74090704e93652
Nicole Lasusa <nicolelasusa@Nicoles-MacBook-Air.local>
2020-08-03 14:13:43 -0400

    patient page

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-// NAV BAR TOGGLE 
-function classToggle() {
-    const navs = document.querySelectorAll('.Navbar__Items')
-    
-    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
-  }
-  
-  document.querySelector('.Navbar__Link-toggle')
-    .addEventListener('click', classToggle);
-// end of Nav bar toggle
+
+
+// page navigation on click events
+$("#patient-seemore").on('click', function(){
+  window.location = "patient.html";    
+});
+
+// end of page navigation on click events// 
 
