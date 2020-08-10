0c0942f9c2412e4c661c09128cb9f44fce89a23f
Nicole Lasusa <nicolelasusa@Nicoles-Air.fios-router.home>
2020-08-10 15:00:11 -0400

    sign up form

--- app.js
+++ app.js
@@ -6,7 +6,14 @@ $("#patient-seemore").on('click', function(){
 });
 
 $("#doctor-login").on('click', function(){
-  window.location = "doctor.html";    
+  window.location = "doctor.html";  
 });
 
+$("#doctor-signup").on('click', function(){
+  window.location = "doctor.html";
+});
+
+
 // end of page navigation on click events// 
+
+
