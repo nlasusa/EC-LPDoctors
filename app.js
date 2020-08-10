576777fab2ec4cba360c671a555720e16f83b9bb
Nicole Lasusa <nicolelasusa@Nicoles-Air.fios-router.home>
2020-08-10 17:08:33 -0400

    added collapsible fact section for patient LP

--- app.js
+++ app.js
@@ -12,8 +12,14 @@ $("#doctor-login").on('click', function(){
 $("#doctor-signup").on('click', function(){
   window.location = "doctor.html";
 });
-
-
 // end of page navigation on click events// 
 
 
+function toggleIcon(e) {
+  $(e.target)
+      .prev('.panel-heading')
+      .find(".more-less")
+      .toggleClass('fa-plus fa-minus');
+}
+$('.panel-group').on('hidden.bs.collapse', toggleIcon);
+$('.panel-group').on('shown.bs.collapse', toggleIcon);
