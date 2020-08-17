6ea024f7bd5b21a20651b6352b11cccd1ac214eb
Nathan Hill <pjsstp@gmail.com>
2020-08-17 13:24:10 -0400

    added mongoose model for doctors, added blank controller module to add database functions, and updated server to connect to database on startup.

---
+++ controller/index.js
@@ -0,0 +1,8 @@
+var mongojs = require("mongojs")
+const db = require("../models")
+
+const controller = {
+    // any DB functions will be written here
+}
+
+module.exports = controller
--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 // Declare our core Dependencies
 // =============================================================
+require("dotenv").config();
 const app = require('./server-config')
+const mongoose = require("mongoose");
+const MONGODB_URI = "mongodb://localhost/dbfor_submarine_2";
+
+// define our port both here and in production
 const port = process.env.PORT || 8000;
+
+// connect to MongoDB
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, (err) => {
+    if (err) {
+        console.log("There is a problem with the connection" + err)
+    } else {
+        console.log("Mongoose connection is good.")
+    }
+});
 
 // Start the server
 app.listen(port, () => console.log(`Listening on port ${port}`));
