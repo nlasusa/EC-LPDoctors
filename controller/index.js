f3e6563abb8475b8c497300574c61dcf2b1933fe
Nathan Hill <pjsstp@gmail.com>
2020-08-19 14:09:25 -0400

    Have working route to take in doctor info as json and add that information to the db, using route, controller, and model functions. Password is encrypted prior to document creation.

--- controller/index.js
+++ controller/index.js
@@ -1,8 +1,55 @@
 var mongojs = require("mongojs")
 const db = require("../models")
+const Doctor = require("../models/Doctor")
 
+// any DB functions will be written in this object here, and sent to the api routes to be used
 const controller = {
-    // any DB functions will be written here
+
+
+    //=====================================================================================================
+    //
+    //  This function creates a new doctor in the database using the db function we wrote to make sure the
+    //    password is encrypted. The function takes in an object which will be a full doctor DB object, 
+    //    including the following fields: 
+    // 
+    //      "firstName" : 
+    //      "lastName" : 
+    //      "officePhone" : 
+    //      "emailAddress" : 
+    //      "websiteURL" : 
+    //      "streetAddress" : 
+    //      "streetAddress2" : 
+    //      "city" : 
+    //      "state" : 
+    //      "zip" : 
+    //      "activeMember" : 
+    //      "membershipDate" :
+    //      "paymentinfo" : 
+    //      "recurringPayment" :  
+    //      "password" :
+    //      "picture" : 
+    // 
+    //    the newly-created object will be returned to the front-end with a success message if the 
+    //    object is created in the database, and with an error message if it is not. 
+    //
+    //=====================================================================================================
+    createNewDoctor : (doctorObj, callback) => {
+        var newDoctor = new Doctor(doctorObj)
+        Doctor.createNew(newDoctor, (err, theDoctor) => {
+            if(err) {
+                callback({
+                    status: "Error",
+                    body: err
+                })
+            }
+            else {
+                callback({
+                    status: "Success",
+                    body : theDoctor
+                })
+            }
+        })
+    }
 }
 
 module.exports = controller
